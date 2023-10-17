ba4904cb0b3a7ebabff31e34c781b58e53bfa21d
Dasaokjn <131093951+Dasaokjn@users.noreply.github.com>
2023-10-17 10:02:26 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -2,3 +2,14 @@ var who65='world';
 function sayhello65(){alert('hello,'+who65+'!')}
 function taro65(){who=65'太郎';}
 function hanako65(){who65='花子'；}
+  function sayhello65() {
+      alert("こんにちは！");
+    }
+
+    function taro65() {
+      alert("太郎が来た");
+    }
+
+    function hanako65() {
+      alert("花子が来た");
+    }
