a7f56ac1996c580a65978d9d367956d0b3d3c3d6
Dasaokjn <131093951+Dasaokjn@users.noreply.github.com>
2023-10-24 10:13:27 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -1,3 +1,14 @@
-function sayhello68(){ alert('Hello,'+document.getElementByld('Who68').innerHTML+'!')}
-function taro68(){document.getElementByld('who68').innerHTML='太郎';}
-function hanako68(){document.getElementByld('who68').inner='花子';}
+
+    function sayhello68() {
+      alert('Hello, ' + document.getElementById('who68').innerHTML + '!');
+    }
+
+    function taro68() {
+      document.getElementById('who68').innerHTML = '太郎';
+      alert('太郎が来た');
+    }
+
+    function hanako68() {
+      document.getElementById('who68').innerHTML = '花子';
+      alert('花子が来た');
+    }
