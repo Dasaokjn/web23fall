2fcc67017e335e664f5b407b293d38d8efedcf29
Dasaokjn <131093951+Dasaokjn@users.noreply.github.com>
2023-10-31 10:18:25 +0900

    Update ex06-88.js

--- ex06-88.js
+++ ex06-88.js
@@ -1,10 +1,10 @@
 function gazo688() {
   var img = document.getElementById('who688').getElementsByTagName('img')[0];
   if (img.getAttribute('alt') === 'オフ') {
-    img.src = 'uresinoryuu./image/bulbon.png'; // 画像のパスを修正
+    img.src = './image/bulbon.png'; 
     img.alt = 'オン';
   } else {
-    img.src = 'Josai./image/bulboff.png'; // 画像のパスを修正
+    img.src = './image/bulboff.png'; 
     img.alt = 'オフ';
   }
 }
