6c93fcdd8975fadfafcc643837ce7309ffbf114d
Dasaokjn <131093951+Dasaokjn@users.noreply.github.com>
2023-11-07 10:38:34 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -2,3 +2,7 @@
             var image = document.getElementById("logo");
             image.src = './image/Josai.png';
         }
+ function imgchange2() {
+            var image = document.getElementById("logo");
+            image.src = './image/Josai.png';
+        }
