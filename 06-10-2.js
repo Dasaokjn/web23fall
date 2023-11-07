1e06e0ca6a4066d22aef371f4741178d3b99769b
Dasaokjn <131093951+Dasaokjn@users.noreply.github.com>
2023-11-07 10:43:22 +0900

    Create 06-10-2.js

---
+++ 06-10-2.js
@@ -0,0 +1,7 @@
+ function imgchange3() {
+            var image = document.getElementById("logo");
+            image.src = './image/Josai.png';
+        } function imgchange4() {
+            var image = document.getElementById("logo");
+            image.src = './image/Josai.png';
+        }
