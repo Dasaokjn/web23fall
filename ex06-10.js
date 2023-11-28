a2fab220772c162d97e0468263058826bf27eb35
Dasaokjn <131093951+Dasaokjn@users.noreply.github.com>
2023-11-28 09:48:27 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,6 +1,6 @@
  function imgchange1() {
             var image = document.getElementById("logo");
-            image.src = './image/sekisei.jpg';
+            image.src = './image/sekisei1.jpg';
         }
  function imgchange2() {
             var image = document.getElementById("logo");
