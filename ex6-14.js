1b5279819766ac41cff7ebbfd6649cd22c961ffa
Dasaokjn <131093951+Dasaokjn@users.noreply.github.com>
2023-12-05 10:20:37 +0900

    Create ex6-14.js

---
+++ ex6-14.js
@@ -0,0 +1,6 @@
+function henkou1() {
+            document.getElementById("gh").src = './image/変更後の画像.jpg';
+        }
+
+        function henkou2() {
+            document.getElementById("gh").src = './image/uresinoryuu.png'; // 画像のパスを変更
