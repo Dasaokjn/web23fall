0fbb1b584773280715a8c264f241e028efc63ec7
Dasaokjn <131093951+Dasaokjn@users.noreply.github.com>
2023-12-19 09:21:41 +0900

    Update ex07-6.js

--- ex07-6.js
+++ ex07-6.js
@@ -1,1 +1,5 @@
+function nazo76() {if (document.getElementById('answer76').value !== '夢') // !==で値か型が等しくないことを指す
+                  {alert('はずれ！');}
+                  else{alert('あたり！');}
+                  }
 
