9dc693fb825491e7f6ea3b025d13dc6524aaa742
Dasaokjn <131093951+Dasaokjn@users.noreply.github.com>
2023-12-19 10:31:37 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,5 +1,5 @@
 function change1() {
-    document.getElementById('ダウンロード').src = './image/ダウンロード.jpg';
+    document.getElementById('ダウンロード').src = './image/ダウンロード.png';
 }
 function change2() {
     document.getElementById('uresinoryuu').src = './image/uresinoryuu.png';
