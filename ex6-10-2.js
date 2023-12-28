f15a626f49424fa46b22c3033e42d7998bc8f7d1
Dasaokjn <131093951+Dasaokjn@users.noreply.github.com>
2023-12-29 08:45:42 +0900

    Update ex6-10-2.js

--- ex6-10-2.js
+++ ex6-10-2.js
@@ -1,7 +1,10 @@
- function imgchange3() {
-            var image = document.getElementById("jo");
-            image.src = './image/bolbon.png';
-        } function imgchange4() {
-            var image = document.getElementById("jo");
-            image.src = './image/bulboff.png';
-        }
+function imgchange3(){
+  document.getElementById('logo').src =
+    './image/uresinoryuu.png';
+}
+
+function imgchange4(){
+  document.getElementById('logo').src =
+    './image/じょさい.png';
+}
+
