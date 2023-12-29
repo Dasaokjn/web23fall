44796a8be414df3c5d8ec68ca911ada555107fb4
Dasaokjn <131093951+Dasaokjn@users.noreply.github.com>
2023-12-29 09:08:24 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,20 +1,18 @@
- <script>
-        function startTimer() {
-            // 3秒後にchange1関数を呼び出す
-            setTimeout(change1, 3000);
-        }
+   function startTimer() {
+      // 3秒後にchange1関数を呼び出す
+      setTimeout(change1, 3000);
+    }
 
-        function change1() {
-            // 画像を変更する
-            document.getElementById('1').src = './image/japan.png';
-            
-            // さらに3秒後にchange2関数を呼び出す
-            setTimeout(change2, 3000);
-        }
+    function change1() {
+      // 画像を変更する
+      document.getElementById('1').src = './image/uresinoryuu.png';
+       
+      // さらに3秒後にchange2関数を呼び出す
+      setTimeout(change2, 3000);
+    }
 
-        function change2() {
-            // 画像を再び変更する
-            document.getElementById('1').src = './image/uresinoryuu.png';
-        }
-    </script>
+    function change2() {
+      // 画像を再び変更する
+      document.getElementById('1').src = './image/japan.png';
+    }
 
